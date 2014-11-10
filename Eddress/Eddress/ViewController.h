e266a7ac67081b70251902c2ee0611402131df73
student <none@none.com>
2014-11-10 10:45:31 -0800

    First Commit! Yay!

---
+++ Eddress/Eddress/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Eddress
+//
+//  Created by student on 11/10/14.
+//  Copyright (c) 2014 Gemmell, Kubota, Teller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Eddress/Eddress/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Eddress
+//
+//  Created by student on 11/10/14.
+//  Copyright (c) 2014 Gemmell, Kubota, Teller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
